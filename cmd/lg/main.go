cbb3640f73c27aa91e4cd934f4505a555ddead27
Dennis Günnewig <dev@fedux.org>
2017-02-01 01:14:48 +0100

    Add config directory to config

--- cmd/lg/main.go
+++ cmd/lg/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/maxmeyer/letter-generator-go/commandline"
 	"github.com/maxmeyer/letter-generator-go/letter_generator"
+	lgos "github.com/maxmeyer/letter-generator-go/os"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
@@ -28,19 +29,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	config_directory := ".lg"
-
 	log.WithFields(log.Fields{
 		"path":   current_directory,
 		"status": "success",
 	}).Debug("Getting current directory")
 
+	home_directory, err := lgos.HomeDirectory()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"msg":    err.Error(),
+			"status": "failure",
+		}).Fatal("Getting home directory of current user")
+
+		os.Exit(1)
+	}
+
+	log.WithFields(log.Fields{
+		"path":   home_directory,
+		"status": "success",
+	}).Debug("Getting home directory of current user")
+
 	config := letter_generator.Config{}
-	config.RemoteSources = []string{"git@gitlab.com:maxmeyer/letter-template.git"}
-	config.RecipientsFile = filepath.Join(current_directory, config_directory, "data/to.json")
-	config.MetadataFile = filepath.Join(current_directory, config_directory, "data/metadata.json")
-	config.SenderFile = filepath.Join(current_directory, config_directory, "data/from.json")
-	config.TemplateFile = filepath.Join(current_directory, config_directory, "templates/letter.tex.tt")
+	config.RemoteSources = []string{filepath.Join(home_directory, ".local/share/letter-template/config.git"), "git@gitlab.com:maxmeyer/letter-template.git"}
+	config.ConfigDirectory = ".lg"
+	config.RecipientsFile = filepath.Join(current_directory, config.ConfigDirectory, "data/to.json")
+	config.MetadataFile = filepath.Join(current_directory, config.ConfigDirectory, "data/metadata.json")
+	config.SenderFile = filepath.Join(current_directory, config.ConfigDirectory, "data/from.json")
+	config.TemplateFile = filepath.Join(current_directory, config.ConfigDirectory, "templates/letter.tex.tt")
 
 	cli := commandline.Cli{}
 	err = cli.Run(os.Args, config, current_directory)
