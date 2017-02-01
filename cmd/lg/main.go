245e742a3700837a44aa08356f20d5c138c59d43
Dennis Günnewig <dev@fedux.org>
2017-02-01 02:17:10 +0100

    Give lg a directory to initialize it

--- cmd/lg/main.go
+++ cmd/lg/main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"github.com/maxmeyer/letter-generator-go/commandline"
-	"github.com/maxmeyer/letter-generator-go/letter_generator"
-	lgos "github.com/maxmeyer/letter-generator-go/os"
-	log "github.com/sirupsen/logrus"
 	"os"
-	"path/filepath"
 )
 
 func init() {
@@ -18,48 +14,8 @@ func init() {
 }
 
 func main() {
-	current_directory, err := os.Getwd()
-
-	if err != nil {
-		log.WithFields(log.Fields{
-			"msg":    err.Error(),
-			"status": "failure",
-		}).Fatal("Getting current directory")
-
-		os.Exit(1)
-	}
-
-	log.WithFields(log.Fields{
-		"path":   current_directory,
-		"status": "success",
-	}).Debug("Getting current directory")
-
-	home_directory, err := lgos.HomeDirectory()
-
-	if err != nil {
-		log.WithFields(log.Fields{
-			"msg":    err.Error(),
-			"status": "failure",
-		}).Fatal("Getting home directory of current user")
-
-		os.Exit(1)
-	}
-
-	log.WithFields(log.Fields{
-		"path":   home_directory,
-		"status": "success",
-	}).Debug("Getting home directory of current user")
-
-	config := letter_generator.Config{}
-	config.RemoteSources = []string{filepath.Join(home_directory, ".local/share/letter-template/.git"), "git@gitlab.com:maxmeyer/letter-template.git"}
-	config.ConfigDirectory = ".lg"
-	config.RecipientsFile = filepath.Join(current_directory, config.ConfigDirectory, "data/to.json")
-	config.MetadataFile = filepath.Join(current_directory, config.ConfigDirectory, "data/metadata.json")
-	config.SenderFile = filepath.Join(current_directory, config.ConfigDirectory, "data/from.json")
-	config.TemplateFile = filepath.Join(current_directory, config.ConfigDirectory, "templates/letter.tex.tt")
-
 	cli := commandline.Cli{}
-	err = cli.Run(os.Args, config, current_directory)
+	err := cli.Run(os.Args)
 
 	if err != nil {
 		os.Exit(1)
--- commandline/parser.go
+++ commandline/parser.go
@@ -1,17 +1,17 @@
 package commandline
 
 import (
-	"fmt"
 	"github.com/maxmeyer/letter-generator-go/letter_generator"
+	lgos "github.com/maxmeyer/letter-generator-go/os"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
 type Cli struct{}
 
-func (p *Cli) Run(args []string, config letter_generator.Config, current_directory string) error {
+func (p *Cli) Run(args []string) error {
 
 	appMetadata := letter_generator.AppMetadata{
 		Version: "0.0.1",
@@ -40,6 +40,15 @@ func (p *Cli) Run(args []string, config letter_generator.Config, current_directo
 	}
 
 	app.Action = func(c *cli.Context) error {
+		var working_directory string
+
+		if c.Args().Get(0) != "" {
+			working_directory = c.Args().Get(0)
+		} else {
+			working_directory = get_current_working_directory()
+		}
+
+		config := build_config(working_directory)
 		parse_global_options(c, config)
 
 		err := build(config)
@@ -63,9 +72,18 @@ func (p *Cli) Run(args []string, config letter_generator.Config, current_directo
 				},
 			},
 			Action: func(c *cli.Context) error {
+				var working_directory string
+
+				if c.Args().Get(0) != "" {
+					working_directory = c.Args().Get(0)
+				} else {
+					working_directory = get_current_working_directory()
+				}
+
+				config := build_config(working_directory)
 				parse_global_options(c, config)
 
-				err := initialize(current_directory, config)
+				err := initialize(working_directory, config)
 
 				if err != nil {
 					return err
@@ -85,6 +103,15 @@ func (p *Cli) Run(args []string, config letter_generator.Config, current_directo
 				},
 			},
 			Action: func(c *cli.Context) error {
+				var working_directory string
+
+				if c.Args().Get(0) != "" {
+					working_directory = c.Args().Get(0)
+				} else {
+					working_directory = get_current_working_directory()
+				}
+
+				config := build_config(working_directory)
 				parse_global_options(c, config)
 
 				err := build(config)
@@ -133,13 +160,54 @@ func parse_global_options(c *cli.Context, config letter_generator.Config) {
 	}
 
 	log.WithFields(log.Fields{
-		"verbose":     c.Bool("verbose"),
-		"show-config": c.Bool("show-config"),
+		"verbose": c.Bool("verbose"),
 	}).Info("Parsing commandline options")
+}
 
-	if c.Bool("show-config") == true {
-		fmt.Print(strings.Join(config.ToString(), "\n"))
-		fmt.Print("\n")
-		os.Exit(0)
+func get_current_working_directory() string {
+	current_directory, err := os.Getwd()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"msg":    err.Error(),
+			"status": "failure",
+		}).Fatal("Getting current directory")
+
+		os.Exit(1)
 	}
+
+	log.WithFields(log.Fields{
+		"path":   current_directory,
+		"status": "success",
+	}).Debug("Getting current directory")
+
+	return current_directory
+}
+
+func build_config(working_directory string) letter_generator.Config {
+	home_directory, err := lgos.HomeDirectory()
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"msg":    err.Error(),
+			"status": "failure",
+		}).Fatal("Getting home directory of current user")
+
+		os.Exit(1)
+	}
+
+	log.WithFields(log.Fields{
+		"path":   home_directory,
+		"status": "success",
+	}).Debug("Getting home directory of current user")
+
+	config := letter_generator.Config{}
+	config.RemoteSources = []string{filepath.Join(home_directory, ".local/share/letter-template/.git"), "git@gitlab.com:maxmeyer/letter-template.git"}
+	config.ConfigDirectory = ".lg"
+	config.RecipientsFile = filepath.Join(working_directory, config.ConfigDirectory, "data/to.json")
+	config.MetadataFile = filepath.Join(working_directory, config.ConfigDirectory, "data/metadata.json")
+	config.SenderFile = filepath.Join(working_directory, config.ConfigDirectory, "data/from.json")
+	config.TemplateFile = filepath.Join(working_directory, config.ConfigDirectory, "templates/letter.tex.tt")
+
+	return config
 }
