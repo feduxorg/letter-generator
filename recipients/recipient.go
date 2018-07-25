168aca9e56518b1b221d45b1026c43b58f14c25a
Dennis Günnewig <dev@fedux.org>
2018-07-25 08:45:37 +0200

    Move to struct

--- recipients/recipient.go
+++ recipients/recipient.go
@@ -1,9 +1,9 @@
 package recipients
 
 type Recipient struct {
-	Name           string   `json:"name"`
-	C_O            string   `json:"c_o"`
-	Street         string   `json:"street"`
-	City           string   `json:"city"`
-	AdditionalInfo []string `json:"additional_info"`
+	Name           string            `json:"name"`
+	C_O            string            `json:"c_o"`
+	Street         string            `json:"street"`
+	City           string            `json:"city"`
+	AdditionalInfo map[string]string `json:"additional_info"`
 }
